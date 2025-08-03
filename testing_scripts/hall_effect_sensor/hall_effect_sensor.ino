0023e67b2e0a318eedaf00978d6be4b589dcd0b3
HBBOUWMAN <112092513+HBBOUWMAN@users.noreply.github.com>
2025-08-03 17:14:51 +0200

    print kmph to SM

--- testing_scripts/hall_effect_sensor/hall_effect_sensor.ino
+++ testing_scripts/hall_effect_sensor/hall_effect_sensor.ino
@@ -1,21 +1,40 @@
 void setup() {
-  Serial.begin(9600);       // Start serial communication
-  pinMode(5, OUTPUT);       // Set digital pin 5 as output
+  Serial.begin(9600);
+  pinMode(5, OUTPUT);
 }
 
-float dt = 0;
+float voltage = 0;
+bool aboveThreshold = false;
+unsigned long lastTriggerTime = 0;
+float timeBetweenTriggers = 0;
+float speed = 0;
 
 void loop() {
-  int analogValue = analogRead(A0);               // Read analog input
-  float voltage = analogValue * (5.0 / 1023.0);   // Convert to voltage
+  int analogValue = analogRead(A0);
+  voltage = analogValue * (5.0 / 1023.0);
+  //Serial.print("Voltage: ");
+  //Serial.println(voltage);
 
-  Serial.println(voltage);                        // Print voltage
-
+  // Check for crossing the 3V threshold
   if (voltage > 3) {
-    digitalWrite(5, HIGH);   // Turn LED on
+    if (!aboveThreshold) {
+      // We just crossed the threshold now
+      unsigned long currentTime = millis();
+      timeBetweenTriggers = currentTime - lastTriggerTime;
+      lastTriggerTime = currentTime;
+      speed = 2/(timeBetweenTriggers/1000);
+
+      Serial.print(speed);
+      Serial.println("KM/h");
+
+      aboveThreshold = true; // Mark that we're now above
+    }
+
+    digitalWrite(5, HIGH); // LED on
   } else {
-    digitalWrite(5, LOW);    // Turn LED off
+    digitalWrite(5, LOW);  // LED off
+    aboveThreshold = false; // Reset state so we can detect next crossing
   }
 
-  delay(100); // Small delay
+  delay(10);
 }
